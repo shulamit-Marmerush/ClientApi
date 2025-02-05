ca09eb8b1c264fbb4285c8395c9f8c135db49099
Shulamit-Marmerush <sm6736243@gmail.com>
2025-02-05 23:09:20 +0200

    change

---
+++ ToDoListReact-master/build/precache-manifest.c7e20e6834a194eb755b9c22c17f9703.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd723561181e5430a084040a8c2b488c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e61c4ec9df5a4cfeaf2f",
+    "url": "/static/js/2.66c277a7.chunk.js"
+  },
+  {
+    "revision": "60788922c7cf2973651d",
+    "url": "/static/js/main.53aa788b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
